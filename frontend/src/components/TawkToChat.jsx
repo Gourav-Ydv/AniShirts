cf5b5907215b693f1629713ab3a49f19f65ad80e
Gourav-Yadav-au52 <gvyadav2012@gmail.com>
2024-05-07 23:16:40 +0530

    new commit

---
+++ frontend/src/components/TawkToChat.jsx
@@ -0,0 +1,22 @@
+// TawkToChat.js
+
+import React, { useEffect } from 'react';
+
+const TawkToChat = () => {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://embed.tawk.to/6623f68c1ec1082f04e50f6c/1hru7m621';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  return null;
+};
+
+export default TawkToChat;
